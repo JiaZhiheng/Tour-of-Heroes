25336f8f7d03093dc4e9f0bdc3352aaab75c3f58
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2023-08-14 17:45:08 +0800

    create hero-form compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   imports: [
@@ -36,6 +37,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent,
+    HeroFormComponent,
   ],
   bootstrap: [AppComponent],
 })
